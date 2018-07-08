f7b21dc30c3e3bafd7b6c684ca979d2116b9752c
Nodragem <geoffrey.megardon@gmail.com>
2018-07-08 13:01:25 +0100

    make the template runnable and add a README

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-solo_template
+## Solo Template
+
+On Windows, open the command line the project folder and run:
+```
+./simple_server.bat
+```
+
+This should result into the following messages:
+```
+REM This is a simple static server, perfect for developping solo games
+REM that will be deployed on a static server.
+REM -----------------------------------------
+python -m SimpleHTTPServer
+Serving HTTP on 0.0.0.0 port 8000 ...
+```
+
+Now open a browser and type the following address:
+- http://localhost:8000
+- http://192.168.1.2:8000
+- http://127.0.0.1:8000
+
+Find more details on Python Simple HTTP Server here:
+
+http://www.linuxjournal.com/content/tech-tip-really-simple-http-server-python
+
+
--- js/game.js
+++ js/game.js
@@ -6,12 +6,13 @@ var Game = {
     preload : function() {
         // Here we load all the needed resources for the level.
         // In our case, that's just two squares - one for the snake body and one for the apple.
-        game.load.image('snake', './assets/images/snake.png');
-        game.load.image('apple', './assets/images/apple.png');
+        game.load.image('player', './assets/images/player.png');
+        game.load.image('enemy', './assets/images/enemy.png');
     },
 
     create : function() {
-        this.add.button(0, 0, 'snake', this.endGame, this);
+        this.add.button(200, 200, 'player', this.endGame, this);
+        this.add.button(400, 200, 'enemy', this.endGame, this);
     },
 
     update: function() {
